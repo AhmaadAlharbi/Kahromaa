f0827a59c212cc6400491a9fb847d30ae8120f2b
ahmad@macMini <ahmaadzaid7@gmail.com>
2024-05-19 21:10:04 +0300

    add social links

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
-document.getElementById("menu-toggle").addEventListener("click", function () {
+// Wait for the DOM to be fully loaded before running the script
+document.addEventListener("DOMContentLoaded", () => {
+  // Get references to the elements
+  const menuToggle = document.getElementById("menu-toggle");
   const navbar = document.getElementById("navbar-default");
-  navbar.classList.toggle("hidden");
-});
+  const contactForm = document.getElementById("contactForm");
+  const submissionMessage = document.getElementById("submissionMessage");
 
-document
-  .getElementById("contactForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent the default form submission
+  // Add event listener for the menu toggle button
+  menuToggle.addEventListener("click", toggleNavbar);
+
+  // Add event listener for the contact form submission
+  contactForm.addEventListener("submit", handleFormSubmission);
+
+  // Function to toggle the visibility of the navbar
+  function toggleNavbar() {
+    navbar.classList.toggle("hidden");
+  }
+
+  // Function to handle the contact form submission
+  function handleFormSubmission(event) {
+    // Prevent the default form submission
+    event.preventDefault();
 
     // Show the submission message
-    document.getElementById("submissionMessage").classList.remove("hidden");
+    submissionMessage.classList.remove("hidden");
 
-    // Optionally, you can add a timeout to simulate the form submission delay
-    setTimeout(function () {
-      document.getElementById("contactForm").submit(); // Submit the form
+    // Simulate form submission delay and then submit the form
+    setTimeout(() => {
+      contactForm.submit(); // Submit the form
     }, 2000); // Adjust the timeout duration as needed
-  });
+  }
+});
